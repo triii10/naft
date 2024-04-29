0dde1fd50f53abf4abbc69462cf16bd25111a483
Trilok Bhattacharya <triii.10@outlook.com>
2024-04-29 00:29:04 +0000

    Implemented flame graph command

--- naft.sh
+++ naft.sh
@@ -43,14 +43,12 @@ prerequisites() {
 
     if [ -d "$folder_path" ]; then
         # Folder exists, move it
-        tar -czf "$new_folder_path.tar.gz" "$folder_path" >/dev/null
+        if [ $argc_password_stdin ] || [ -z $argc_password ]; then
+            sudo --prompt='' -S tar -czf "$new_folder_path.tar.gz" "$folder_path" >/dev/null
+        else
+            base64 -d $argc_password | sudo --prompt='' -S tar -czf "$new_folder_path.tar.gz" "$folder_path" >/dev/null
+        fi
         rm -rf $folder_path >/dev/null
-    fi
-
-    # Move the perf record output 
-    if [ -f "perf.data" ]; then 
-        mv "perf.data" $new_folder_path/perf.data >/dev/null
-        echo_success "Existing perf.data moved to $new_folder_path/perf.data"
     fi
 
     if [ -d "$new_folder_path" ]; then
@@ -72,6 +70,21 @@ prerequisites() {
     return 0;
 }
 
+start_perf() {
+    # Too lazy to handle errors in this function
+    local event=$1
+    local qemu_pid=$(pgrep qemu)
+    if [ $argc_password_stdin ] || [ -z $argc_password ]; then
+        sudo --prompt='' -S perf record -g --quiet -p $qemu_pid -o perf.$event -F 99 -- sleep $argc_perf_period &
+    else
+        base64 -d $argc_password | sudo --prompt='' -S perf record -g --quiet -p $qemu_pid -o perf.$event -F 99 -- sleep $argc_perf_period &
+    fi
+    # perf_pid=$(pgrep perf -n)
+    spinner_wait $! "Waiting for perf to terminate"
+    echo_success "perf captured to file perf.$event"
+    return 0
+}
+
 start_fio() {
     # The remote path is /tmp. Modify to change
     remote_path="/tmp/results"
@@ -79,14 +92,18 @@ start_fio() {
     ssh "$remote_server" "mkdir -p $remote_path" >/dev/null
 
     # Execute commands over SSH
+    # Either send the password in a text file, or provide it in stdin.
+    # Yes, I know it's not secure, but it does the job for me in an internal system
+
     if [ $argc_domain_password_stdin ] || [ -z $argc_domain_password ]; then
+        fio_clock=$(date +%s)
         fio_pid=$(ssh -tq "$remote_server" "sudo -S --prompt='' bash -c 'nohup fio --minimal $remote_path/$argc_fio_ini & echo \$!' >/dev/null" >/dev/null)
     else
+        fio_clock=$(date +%s)
         fio_pid=$(ssh -tq "$remote_server" "base64 -d $argc_domain_password | sudo --prompt='' -S bash -c 'nohup fio --minimal $remote_path/$argc_fio_ini & echo \$!' >/dev/null" >/dev/null)
     fi
     fio_pid=$(ssh "$remote_server" "pgrep fio -n")
 
-    fio_clock=$(($(date +%s) + 0))
     echo '' > /tmp/fio_snap
     echo '' > /tmp/fio_snap_merge
     
@@ -103,7 +120,7 @@ start_fio() {
 take_single_snap() {
     local name="snap-$1"
     local present_dir=$(pwd)
-    virsh snapshot-create-as --domain $argc_domain $name --diskspec vda,file=$present_dir/$name --disk-only --no-metadata >/dev/null
+    virsh snapshot-create-as --domain $argc_domain $name --diskspec $argc_block_device,file=$present_dir/$name --disk-only --no-metadata >/dev/null
     
     if [ $? -eq 0 ]; then
         echo "$(($(date +%s) - $fio_clock))" >> /tmp/fio_snap
@@ -177,6 +194,9 @@ exit_routines() {
         return 1
     fi
 
+    # Copy the perf files to results folder
+    mv perf.* results/
+
     return 0
 }
 
@@ -193,6 +213,7 @@ exit_routines() {
 # @option   --domain-password           File containing the domain password in base64 format
 # @flag     --domain-password-stdin     Provide domain password password in stdin
 # @option   --domain-username           Username of the domain user to ssh and scp
+# @option   --perf-period=10            How many seconds to run perf
 benchmark() {
 
     # Execute the prerequisites
@@ -215,11 +236,15 @@ benchmark() {
     fi
 
     if [[ $argc_snapshots -ne 0 ]]; then
+        start_perf "before_snapshot"
         take_snapshots;
         if [ $? -eq 1 ]; then
             echo_failure "Failure taking snapshots. Exiting.."
             return 1
+        else
+            start_perf "after_snapshot"
         fi
+        
     fi
 
     if [[ $argc_block_commit -ne 0 ]]; then
@@ -233,6 +258,33 @@ benchmark() {
         echo_failure "Exit routines failed. Exiting.."
         return 1
     fi
+}
+
+# @cmd    Generate flame graphs from perf record outputs
+# @option --stackcollapse=$HOME/bin         Location of stackcollapse and flamegraph programs
+flame() {
+
+    # Get all files with the prefix "perf." in the current directory
+    file_list=()
+    for file in ./perf.*; do
+        # Check if the file exists
+        if [ -f "$file" ]; then
+            # Add the file to the array
+            file_list+=("$file")
+        fi
+    done
+
+    # For each file, generate the flame graph
+    for file in "${file_list[@]}"; do
+        if [ $argc_password_stdin ] || [ -z $argc_password ]; then
+            sudo --prompt='' -S perf script -i $file > $file.stack
+        else
+            base64 -d $argc_password | sudo --prompt='' -S perf script -i $file > $file.stack
+        fi
+        stackcollapse-perf.pl --all $file.stack | flamegraph.pl > $file.flame.svg
+    done
+    echo_success "Flame graphs created"
+
 }
 
 # @cmd  Run both benchmark and visualise commands
@@ -249,6 +301,7 @@ benchmark() {
 # @flag     --domain-password-stdin     Provide domain password password in stdin
 # @option   --domain-username           Username of the domain user to ssh and scp
 # @option   --python-venv!              Location to python virtual environment to generate graphs
+# @option   --perf-period=10            How many seconds to run perf
 run() {
     benchmark;
     visualize;
